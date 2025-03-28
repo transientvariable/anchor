aeb5afddc043a7aed761f85b318b700236ae9dd5
transientvariable <transientvariable@gmail.com>
2025-03-28 16:17:22 -0400

    Renamed module: support -> support-go

--- units.go
+++ units.go
@@ -2,7 +2,6 @@ package support
 
 const (
 	// SI size units
-
 	KB = 1000
 	MB = 1000 * KB
 	GB = 1000 * MB
@@ -10,7 +9,6 @@ const (
 	PB = 1000 * TB
 
 	// IEC size units
-
 	KiB = 1024
 	MiB = 1024 * KiB
 	GiB = 1024 * MiB
